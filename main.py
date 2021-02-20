3a02d2159d4993325890a71a2098e247c42c81d5
ashwinhprasad <ashwinprasad202@gmail.com>
2021-02-20 23:58:01 +0530

    task2

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder
 
 # header
 st.markdown("""
-    # Streamlit Decision Trees
-    This App is a streamlit app for performing a simple classification task
-    using random forest and to check which parameters give us an optimized
+    # Prediction Using Decision Tree
+    This is a simple dashboard for performing a simple classification task
+    using Decision Tree Algorithm and to check which parameters give us an optimized
     result
 """)
 
 # display data
-data = load_iris(as_frame=True)
-x = data['data']
-y = data['target']
+data = pd.read_csv("./data/Iris.csv")
+x = data.iloc[:,1:5]
+y = data.iloc[:,5]
 
 if st.button("Display Sample Data"):
     st.markdown("""## Sample Data""")
@@ -26,25 +26,45 @@ if st.button("Display Sample Data"):
 
 # parameter
 st.sidebar.header("Parameters")
-n_estimators = st.sidebar.slider("n_estimators",1,200)
+criterion = st.sidebar.selectbox("Select Your Criterion",["gini","entropy"])
 
 # train test split
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=123)
 
 # training
-clf = RandomForestClassifier(n_estimators=n_estimators)
+clf = DecisionTreeClassifier(criterion=criterion)
 clf.fit(x_train,y_train)
 y_pred  = clf.predict(x_test)
 acc = (y_pred == y_test).mean()*100
-st.write(f"The Accuracy of the model is {acc:.2f}%")
+st.write(f"The Accuracy of the {criterion} based Decision Tree is {acc:.2f}%")
+
+# vectorizing the prediction
+le = LabelEncoder()
+y_pred = le.fit_transform(y_pred)
 
 # making plot
 pca = PCA(2)
 x_test_d = pca.fit_transform(x_test)
-
 fig = plt.figure()
 plt.scatter(x_test_d[:,0],x_test_d[:,1],c=y_pred)
 st.pyplot(fig)
+
+# decision tree plot
+st.write("## Tree Visualization ")
+fig = plt.figure(figsize=(5,10),dpi=100)
+plot_tree(clf,filled=True)
+st.pyplot(fig)
+
+# new prediction
+new_sepalLength = st.number_input(label="Sepal Length")
+new_sepalWidth = st.number_input(label="Sepal Width")
+new_petalLength = st.number_input(label="Petal Length")
+new_petalWidth = st.number_input(label="Petal Width")
+
+#make prediction
+x_test = np.array([new_petalLength,new_sepalWidth,new_petalLength,new_petalWidth])
+st.write(clf.predict(x_test.reshape(-1,4)))
+
 
 # github directory for code
 st.markdown("""
